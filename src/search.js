ea1c59772eea0a87cecd3a63f20fa5185f6d3a48
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-04-23 12:17:47 -0400

    Add import for forecast, change fetch to forecast from day

--- src/search.js
+++ src/search.js
@@ -1,4 +1,5 @@
-import { buildDaily } from "./daily"
+import buildDaily from "./daily";
+import buildForecast from "./forecast.js";
 
 const acceptButton = document.getElementById("acceptInput");
 acceptButton.addEventListener("click", getWeather);
@@ -6,15 +7,16 @@ acceptButton.addEventListener("click", getWeather);
 async function getWeather() {
     let location = document.getElementById("locationInput").value;
     document.getElementById("locationInput").value = "";
-    // Retrieve current weather and build the days display
     try {
-        const responseCurrent = await fetch(
-            `http://api.weatherapi.com/v1/current.json?key=5cd47542065f4defbfc174337241004&q=${location}&aqi=no`,
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/forecast.json?key=5cd47542065f4defbfc174337241004&q=${location}&aqi=no&days=6`,
             { mode: "cors" }
         );
-        const currentWeatherData = await responseCurrent.json();
-        buildDaily(currentWeatherData);
+        const data = await response.json();
+        buildDaily(data);
+        buildForecast(data);
+        console.log(data);
     } catch (error) {
-        console.error("Error: ", error);
-    }
+        console.error("Error: ", error)
+    };
 }
