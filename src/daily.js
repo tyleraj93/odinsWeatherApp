09948ca4a00650758ad9ad2def08b97c9573edcd
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-05-09 15:20:49 -0400

    Add correct units to the display

--- src/daily.js
+++ src/daily.js
@@ -1,20 +1,22 @@
-export default function buildDaily(data) {
+export default function buildDaily(data, tempUnit) {
     const dailyDisplay = document.getElementById("dailyWeather");
+    
 
     if (dailyDisplay.textContent) {
         dailyDisplay.textContent = "";
     }
 
     const temp = document.createElement("h1");
-    temp.textContent = `${data.current.temp_f}\u00B0 f`;
+    temp.textContent = `${data.current["temp_" + tempUnit]}\u00B0 ${tempUnit}`;
     dailyDisplay.appendChild(temp);
 
     const location = document.createElement("h2");
+    location.id = "location";
     location.textContent = `${data.location.name}, ${data.location.region}`;
     dailyDisplay.appendChild(location);
 
     const feelsLike = document.createElement("p");
-    feelsLike.textContent = `Feels like: ${data.current.feelslike_f}`;
+    feelsLike.textContent = `Feels like: ${data.current["feelslike_" + tempUnit]}\u00B0${tempUnit}`;
     dailyDisplay.appendChild(feelsLike);
 
     const condition = document.createElement("p");
@@ -26,12 +28,14 @@ export default function buildDaily(data) {
     dailyDisplay.appendChild(conditionImg);
 
     const humidity = document.createElement("p");
-    humidity.textContent = `Humidity: ${data.current.humidity}`;
+    humidity.textContent = `Humidity: ${data.current.humidity}%`;
     dailyDisplay.appendChild(humidity);
 
     const wind = document.createElement("p");
-    wind.textContent = `Wind: ${data.current.wind_mph} ${data.current.wind_dir}`;
+     wind.textContent = `Wind: ${
+         tempUnit === "f"
+             ? data.current.wind_mph + " mph"
+             : data.current.wind_kph + " kph"
+     } ${data.current.wind_dir}`;
     dailyDisplay.appendChild(wind);
-
-    console.log(data);
 }
