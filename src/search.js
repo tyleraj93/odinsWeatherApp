28c49ebaa4413b46f934df7589536e8ace545d67
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-05-09 15:17:51 -0400

    Finish search functionality and add comments

--- src/search.js
+++ src/search.js
@@ -1,22 +1,55 @@
 import buildDaily from "./daily";
 import buildForecast from "./forecast.js";
 
-const acceptButton = document.getElementById("acceptInput");
-acceptButton.addEventListener("click", getWeather);
+// Ensure local storage is empty before starting
+localStorage.setItem("location", "");
 
+const searchButton = document.getElementById("acceptInput");
+
+// Add funcitonality to search button and start the unit converter
+searchButton.addEventListener("click", getWeather);
+searchButton.addEventListener("click", activateUnitConvert);
+
+// Gathers weather data and gives data to helper functions for weather forecast and display
 async function getWeather() {
-    let location = document.getElementById("locationInput").value;
-    document.getElementById("locationInput").value = "";
+    const tempUnit = document.querySelector(
+        "input[name='temperatureUnit']:checked"
+    ).value; // Selects the checked "F" or "C" for the temperature units
+
+    // Stores user input from the text input unless one is in localStorage already
+    let location;
+    if (document.getElementById("locationInput").value) {
+        location = document.getElementById("locationInput").value;
+        localStorage.setItem("location", location);
+    } else {
+        location = localStorage.getItem("location"); // Location stored on localStorage already for fahrenheit and celcius converter
+    }
+
+    // Guard clause to exit function if no user input for location
+    if (!location) {
+        return;
+    }
+
+    document.getElementById("locationInput").value = ""; // Empty the location text input after search is entered
+
     try {
+        // Makes an api request to weatherapi using the provided location
         const response = await fetch(
-            `http://api.weatherapi.com/v1/forecast.json?key=5cd47542065f4defbfc174337241004&q=${location}&aqi=no&days=6`,
+            `http://api.weatherapi.com/v1/forecast.json?key=5cd47542065f4defbfc174337241004&q=${location}&aqi=no&days=3`,
             { mode: "cors" }
         );
         const data = await response.json();
-        buildDaily(data);
-        buildForecast(data);
-        console.log(data);
+        buildDaily(data, tempUnit); // Update the daily weather display
+        buildForecast(data, tempUnit); // Update the forecast display
     } catch (error) {
-        console.error("Error: ", error)
-    };
+        console.error("Error: ", error);
+    }
+}
+
+// Adds the getWeather function to the unit radio inputs to convert the displayed weather units
+function activateUnitConvert() {
+    const fahrenheitInput = document.getElementById("fahrenheit");
+    const celsiusInput = document.getElementById("celsius");
+    fahrenheitInput.addEventListener("click", getWeather);
+    celsiusInput.addEventListener("click", getWeather);
 }
