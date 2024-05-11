36105a2647c34f0379a9ecded9662cf3d201e45e
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-05-11 14:11:29 -0400

    Add inline comments

--- src/forecast.js
+++ src/forecast.js
@@ -1,4 +1,7 @@
+// This function builds the forecast section of the page.
+// It takes in the forecast data and unit of measurement as arguments.
 export default function buildForecast(data, unit) {
+    // Selects the forecast section.
     const forecastDisplay = document.getElementById("forecast");
 
     if (forecastDisplay.textContent) {
@@ -7,11 +10,14 @@ export default function buildForecast(data, unit) {
 
     let forecastDays = data.forecast.forecastday;
 
+    // Calls build future day function for each forecast day.
     for (const day of forecastDays) {
         buildFutureDay(day, unit, forecastDisplay);
     }
 }
 
+// This function builds a forecast day.
+// It takes in the forecast data, unit of measurement, and entry point as arguments.
 function buildFutureDay(data, unit, entryPoint) {
 
     const dayDisplay = document.createElement("div");
@@ -20,14 +26,17 @@ function buildFutureDay(data, unit, entryPoint) {
     conditionImg.src = data.day.condition.icon;
     dayDisplay.appendChild(conditionImg);
 
+    // Uses slice to display the day of the week without the year.
     const date = document.createElement("p");
     date.textContent = data.date.slice(5);
     dayDisplay.appendChild(date);
 
+    // Uses the unit parameter to determine imperial or metric system.
     const maxTemp = document.createElement("p");
     maxTemp.textContent = `High: ${data.day["maxtemp_" + unit]}\u00B0 ${unit}`;
     dayDisplay.appendChild(maxTemp);
 
+    // Uses the unit parameter to determine imperial or metric system.
     const minTemp = document.createElement("p");
     minTemp.textContent = `Low: ${data.day["mintemp_" + unit]}\u00B0 ${unit}`;
     dayDisplay.appendChild(minTemp);
