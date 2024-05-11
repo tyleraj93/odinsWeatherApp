312675d9fe71b061791efad6f8b73e5a96076ac0
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-05-11 14:11:48 -0400

    Add inline comments

--- src/daily.js
+++ src/daily.js
@@ -1,11 +1,17 @@
+// This function constructs and displays daily weather information in the webpage.
+// It accepts two parameters:
+// data - an object containing weather data fetched from an API.
+// tempUnit - a string representing the temperature unit ("c" for Celsius or "f" for Fahrenheit).
+
 export default function buildDaily(data, tempUnit) {
     const dailyDisplay = document.getElementById("dailyWeather");
-    
 
+    // Clear any existing content in the daily weather display.
     if (dailyDisplay.textContent) {
         dailyDisplay.textContent = "";
     }
 
+    // Create and append daily weather temp using the tempUnit parameter.
     const temp = document.createElement("h1");
     temp.textContent = `${data.current["temp_" + tempUnit]}\u00B0 ${tempUnit}`;
     dailyDisplay.appendChild(temp);
@@ -16,7 +22,9 @@ export default function buildDaily(data, tempUnit) {
     dailyDisplay.appendChild(location);
 
     const feelsLike = document.createElement("p");
-    feelsLike.textContent = `Feels like: ${data.current["feelslike_" + tempUnit]}\u00B0${tempUnit}`;
+    feelsLike.textContent = `Feels like: ${
+        data.current["feelslike_" + tempUnit]
+    }\u00B0${tempUnit}`;
     dailyDisplay.appendChild(feelsLike);
 
     const condition = document.createElement("p");
@@ -31,11 +39,12 @@ export default function buildDaily(data, tempUnit) {
     humidity.textContent = `Humidity: ${data.current.humidity}%`;
     dailyDisplay.appendChild(humidity);
 
+    // Create and append daily weather wind using the tempUnit parameter to determine imperial or metric system.
     const wind = document.createElement("p");
-     wind.textContent = `Wind: ${
-         tempUnit === "f"
-             ? data.current.wind_mph + " mph"
-             : data.current.wind_kph + " kph"
-     } ${data.current.wind_dir}`;
+    wind.textContent = `Wind: ${
+        tempUnit === "f"
+            ? data.current.wind_mph + " mph"
+            : data.current.wind_kph + " kph"
+    } ${data.current.wind_dir}`;
     dailyDisplay.appendChild(wind);
 }
